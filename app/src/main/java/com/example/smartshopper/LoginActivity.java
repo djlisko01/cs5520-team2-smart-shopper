1c3cc74001ea0ff5f7578f2435006f9969356e46
klord940 <kristina.lord@maine.edu>
2022-11-17 19:27:33 -0400

    removed unused import

--- app/src/main/java/com/example/smartshopper/LoginActivity.java
+++ app/src/main/java/com/example/smartshopper/LoginActivity.java
@@ -1,16 +1,8 @@
 package com.example.smartshopper;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
   @Override
