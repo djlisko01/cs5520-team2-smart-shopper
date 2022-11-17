616f860d0a3ce2e626cea660998c2c9fa9f6d924
klord940 <kristina.lord@maine.edu>
2022-11-17 19:26:29 -0400

    added create account layout

--- app/src/main/java/com/example/smartshopper/CreateAccountActivity.java
+++ app/src/main/java/com/example/smartshopper/CreateAccountActivity.java
@@ -8,6 +8,6 @@ public class CreateAccountActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_forgot_password);
+    setContentView(R.layout.activity_create_account);
   }
 }
--- app/src/main/java/com/example/smartshopper/LoginActivity.java
+++ app/src/main/java/com/example/smartshopper/LoginActivity.java
@@ -13,21 +13,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
-  DrawerLayout drawerLayout;
-  ActionBarDrawerToggle actionBarDrawerToggle;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
-    // https://www.geeksforgeeks.org/navigation-drawer-in-android/
-    // drawer layout instance to toggle the menu icon to open drawer and back button to close drawer
-    drawerLayout = findViewById(R.id.home_page_drawer_layout);
-    actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-    // pass the Open and Close toggle for the drawer layout listener to toggle the button
-    drawerLayout.addDrawerListener(actionBarDrawerToggle);
-    actionBarDrawerToggle.syncState();
-    // to make the Navigation drawer icon always appear on the action bar
-    Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
   }
 
   public void sendToForgotPasswordActivity(View view) {
@@ -35,16 +24,9 @@ public class LoginActivity extends AppCompatActivity {
     startActivity(forgotPasswordIntent);
   }
 
-  @Override
-  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-    if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
+  public void sendToCreateAccountActivity(View view) {
+    Intent createAccountIntent = new Intent(this, CreateAccountActivity.class);
+    startActivity(createAccountIntent);
   }
 
-  public void sendToLoginActivity(MenuItem item) {
-    Intent loginIntent = new Intent(this, LoginActivity.class);
-    startActivity(loginIntent);
-  }
 }
