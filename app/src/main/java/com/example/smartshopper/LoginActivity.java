5f1725f0dcfad0daf3e5030f49c9c69981e264e6
klord940 <kristina.lord@maine.edu>
2022-11-16 21:36:35 -0400

    added forgot password layout

--- app/src/main/java/com/example/smartshopper/LoginActivity.java
+++ app/src/main/java/com/example/smartshopper/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.smartshopper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -8,6 +11,10 @@ public class LoginActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+  }
 
+  public void sendToForgotPasswordActivity(View view) {
+    Intent forgotPasswordIntent = new Intent(this, ForgotPasswordActivity.class);
+    startActivity(forgotPasswordIntent);
   }
 }
