4143eee1f5dbb7751a22c8dfd78b23da6eaa198f
jyp17 <jypark1024@gmail.com>
2022-11-18 12:34:51 -0800

    Create Saved Deals layout

---
+++ app/src/main/java/com/example/smartshopper/SavedDealsActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartshopper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavedDealsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_deals);
+    }
+}
