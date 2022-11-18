c34054deddb9127996cf7e30113b98615a82d975
Daniel Lisko <djlisko01@gmail.com>
2022-11-17 21:09:29 -0800

    Made a method for showing picasso img; Img loads in deal detail.

--- app/src/main/java/com/example/smartshopper/DealDetails.java
+++ app/src/main/java/com/example/smartshopper/DealDetails.java
@@ -2,20 +2,30 @@ package com.example.smartshopper;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
+import android.widget.ImageView;
+
+import com.example.smartshopper.common.ImageLoader;
 import com.example.smartshopper.models.Deal;
+import com.squareup.picasso.Picasso;
 
 public class DealDetails extends AppCompatActivity {
+
+    ImageView iv_deal_img;
+    ImageLoader imgLoader = new ImageLoader();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deal_details);
 
+        iv_deal_img = findViewById(R.id.iv_deal);
+
         if (getIntent().getExtras() != null) {
             Deal deal = (Deal) getIntent().getSerializableExtra("dealItem");
-            Log.v("DEAL_PAGE", deal.getTitle());
+        //  This works, but maybe a better way is to pass a picasso obj.
+        Picasso picasso = imgLoader.loadPicassoImg(this, deal.getProductImg(), iv_deal_img);
+
         }
     }
 }
