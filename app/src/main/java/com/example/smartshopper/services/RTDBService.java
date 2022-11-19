3892099546f8fedbad0d7d00fa191593aa08c397
Michael Chang <michaelchang106@yahoo.com>
2022-11-19 14:28:19 -0800

    upvote and downvote implemented

--- app/src/main/java/com/example/smartshopper/services/RTDBService.java
+++ app/src/main/java/com/example/smartshopper/services/RTDBService.java
@@ -1,19 +1,12 @@
 package com.example.smartshopper.services;
 
-import androidx.annotation.NonNull;
-
 import com.example.smartshopper.common.Constants;
 import com.example.smartshopper.models.Comment;
 import com.example.smartshopper.models.Deal;
 import com.example.smartshopper.models.User;
-import com.example.smartshopper.responseInterfaces.BoolInterface;
-import com.example.smartshopper.responseInterfaces.DealInterface;
-import com.example.smartshopper.responseInterfaces.ObjectInterface;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.ServerValue;
 
 public class RTDBService {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -101,38 +94,19 @@ public class RTDBService {
         database.getReference().child(Constants.DEALS).child(dealID).child(Constants.COMMENTS).push().setValue(comment);
     }
 
-
-    // Use this to determine whether item exists (bool) for particular query
-    public void getStatusResponseFromQuery(Query query, BoolInterface boolInterface) {
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                boolInterface.onCallback(snapshot.exists());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
-        });
+    public void upVoteDeal(String dealId) {
+        database.getReference()
+                .child(Constants.DEALS)
+                .child(dealId)
+                .child(Constants.UPVOTES)
+                .setValue(ServerValue.increment(1));
     }
 
-    // Use this to get a single response (object) from a query, may contain children
-    public void getDataResponseFromQuery(Query query, ObjectInterface objectInterface) {
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    objectInterface.onCallback(snapshot.getValue());
-                } else {
-                    objectInterface.onCallback(null);
-                }
-            }
-
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
-        });
+    public void downVoteDeal(String dealId) {
+        database.getReference()
+                .child(Constants.DEALS)
+                .child(dealId)
+                .child(Constants.DOWNVOTES)
+                .setValue(ServerValue.increment(1));
     }
-
 }
