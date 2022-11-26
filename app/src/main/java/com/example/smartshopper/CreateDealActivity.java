966d8e562eaecfe71a997c3bc81e3b7f63186076
Brad Egan <bradegan@gmail.com>
2022-11-26 15:14:43 -0500

    Create Deal Button Launch + XML + Activity

---
+++ app/src/main/java/com/example/smartshopper/CreateDealActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartshopper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateDealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_deal);
+    }
+}
